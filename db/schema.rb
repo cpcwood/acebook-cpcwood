565d89ddf5e01795c5183fe8313fd7efe9369d35
cpcwood <cpcwood@outlook.com>
2020-02-22 19:51:17 +0000

    remove pictures from database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_121916) do
+ActiveRecord::Schema.define(version: 2020_02_22_194146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,11 +49,6 @@ ActiveRecord::Schema.define(version: 2020_02_21_121916) do
     t.datetime "updated_at", null: false
     t.bigint "conversation_id"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-  end
-
-  create_table "pictures", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
