5dff5d2b58c94f1b7e5c1c5b8497864082021dd2
cpcwood <cpcwood@outlook.com>
2020-04-08 19:57:24 +0100

    update credentials for linux server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.web_socket_server_url = 'wss://dosdosdesperadosdynamicos.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['https://dosdosdesperadosdynamicos.herokuapp.com', 'http://dosdosdesperadosdynamicos.herokuapp.com']
 end
